eeb7b9d33aa48f08323a28ab6b18e72a222f7395
NeoDarkness <aldi.45ckr@gmail.com>
2017-07-04 13:07:10 +0700

    wt88047: Initial omni bringup

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -51,9 +51,9 @@ LOCAL_CFLAGS += -DDEFAULT_DENOISE_MODE_ON
 LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../stack/common \
         frameworks/native/include/media/openmax \
-        $(call project-path-for,qcom-display)/libgralloc \
-        $(call project-path-for,qcom-display)/libqdutils \
-        $(call project-path-for,qcom-media)/libstagefrighthw \
+        hardware/qcom/display-caf-msm8916/libgralloc \
+        hardware/qcom/display-caf-msm8916/libqdutils \
+        hardware/qcom/media-caf-msm8916/libstagefrighthw \
         system/media/camera/include \
         $(LOCAL_PATH)/../../mm-image-codec/qexif \
         $(LOCAL_PATH)/../../mm-image-codec/qomx_core \
@@ -67,9 +67,9 @@ LOCAL_CFLAGS += -DUSE_KK_CODE
 endif
 
 #ifeq ($(TARGET_USE_VENDOR_CAMERA_EXT),true)
-#LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
+#LOCAL_C_INCLUDES += hardware/qcom/display-caf-msm8916/libgralloc
 #else
-LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
+LOCAL_C_INCLUDES += hardware/qcom/display-caf-msm8916/libgralloc
 #endif
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
@@ -85,7 +85,7 @@ ifeq ($(TARGET_TS_MAKEUP),true)
 LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
 endif
 LOCAL_MODULE_RELATIVE_PATH    := hw
-LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := camera.msm8916
 LOCAL_32_BIT_ONLY := true
 LOCAL_MODULE_TAGS := optional
 
